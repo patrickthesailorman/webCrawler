e848dc4f7a6e085df5aa92a681d156839777cc92
Patrick McCarty <patrickthesailorman@gmail.com>
2018-05-09 23:31:28 +0000

    Add .gitignore

--- crawler.js
+++ crawler.js
@@ -1,3 +1,3 @@
 var request = require('request');
-var cheerio = require('cheerio');
+var cheerio = require('cheerio'); // use cheerio to parse the page body and assign it to the variable $
 var URL = require('url-parse');
