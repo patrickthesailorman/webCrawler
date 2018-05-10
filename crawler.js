326c3e1af59efc2714f50e6ae5ef78b5a3382ebd
Patrick McCarty <patrickthesailorman@gmail.com>
2018-05-10 00:59:13 +0000

    Add variables

--- crawler.js
+++ crawler.js
@@ -1,3 +1,29 @@
 var request = require('request');
-var cheerio = require('cheerio'); // use cheerio to parse the page body and assign it to the variable $
+var cheerio = require('cheerio');
 var URL = require('url-parse');
+
+var START_URL = "http://foo.bar.com";
+
+var pagesVisited = {};
+var pagesToVisit = [];
+var url = new URL(START_URL);
+var skipped = [];
+var duplicate = [];
+var invalid = [];
+
+
+pagesToVisit.push(START_URL);
+crawl();
+
+function crawl() {
+    var nextPage = pagesToVisit.pop();
+      if (nextPage in pagesVisited) {
+        // We've already visited this page, so push to duplicate and repeat the crawl
+        duplicate.push(nextpage);
+        crawl();
+      } else {
+        // New page we haven't visited
+        visitPage(nextPage, crawl);
+      }
+}
+
